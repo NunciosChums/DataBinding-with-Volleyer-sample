79f598abf2987ef085ac1767fa7aee6f1953f681
Changhoon <susemi99@gmail.com>
2015-09-25 18:07:28 +0900

    add listview adapter

--- app/src/main/java/kr/susemi99/databindingsample/MainActivity.java
+++ app/src/main/java/kr/susemi99/databindingsample/MainActivity.java
@@ -3,9 +3,14 @@ package kr.susemi99.databindingsample;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import kr.susemi99.databindingsample.adapters.ClassListAdapter;
 
 public class MainActivity extends AppCompatActivity
 {
+  private ClassListAdapter adapter;
+
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
@@ -13,5 +18,19 @@ public class MainActivity extends AppCompatActivity
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+
+    adapter = new ClassListAdapter();
+
+    ListView list = (ListView) findViewById(android.R.id.list);
+    list.setAdapter(adapter);
+
+    load();
+  }
+
+  private void load()
+  {
+    adapter.clear();
+    adapter.notifyDataSetChanged();
+    
   }
 }
